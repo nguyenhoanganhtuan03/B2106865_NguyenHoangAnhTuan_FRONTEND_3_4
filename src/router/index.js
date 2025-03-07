8a6ac9bc2c2973472a4e5645ab69095f0fd9c2fc
Nguyễn Hoàng Anh Tuấn <127226234+nguyenhoanganhtuan03@users.noreply.github.com>
2025-03-07 23:09:56 +0700

    Tao trang them lien he moi

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,13 @@ const routes = [
     props: true // Truyền các biến trong $router.params vào làm props
   },
 
+  {
+    path: "/contacts",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+    props: true // Truyền các biến trong $router.params vào làm props
+  },
+
 ];
 
 const router = createRouter({
